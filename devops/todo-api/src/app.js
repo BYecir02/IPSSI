7ec9740ce6db0cf40348d295c4f19851338359f0
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2026-06-04 17:30:43 +0200

    Phase 5 : implémentation de l'endpoint /metrics pour le monitoring Prometheus

--- devops/todo-api/src/app.js
+++ devops/todo-api/src/app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const promClient = require('prom-client');
 const taskRoutes = require('./routes/tasks');
 const errorHandler = require('./middleware/errorHandler');
 const { initDb } = require('./db');
 
 const app = express();
+
+// Monitoring - Phase 5
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ register: promClient.register });
+
+const httpRequestCounter = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
+});
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status: res.statusCode
+    });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 // Initialize Database
 initDb();
